4fb305c3c7f38d5cfd90845fb51b9c572d671708
Kaushal <kaushalpohekar1@gmail.com>
2025-07-24 17:53:39 +0530

    Kaushal

--- server.js
+++ server.js
@@ -10,7 +10,6 @@
 require('dotenv').config();
 const fs = require('fs');
 const https = require('https');
-const http = require('http');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3300;
@@ -23,11 +22,4 @@ const sslOptions = {
 
 https.createServer(sslOptions, app).listen(PORT, HOST, () => {
   console.log(`✅ HTTPS Server running on https://${HOST}:${PORT}`);
-});
-
-http.createServer((req, res) => {
-  res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-  res.end();
-}).listen(80, () => {
-  console.log('🌐 HTTP redirecting to HTTPS');
 });
