cb69e90349143e9af02c87c34aff999aaaa08771
Kaushal <kaushalpohekar1@gmail.com>
2025-07-29 15:42:47 +0530

    New Condtiitons

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 3300;
-const HOST = '0.0.0.0';
-
-app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
-});
 // require('dotenv').config();
-// const fs = require('fs');
-// const https = require('https');
 // const app = require('./app');
 
 // const PORT = process.env.PORT || 3300;
 // const HOST = '0.0.0.0';
 
-// const sslOptions = {
-//   key: fs.readFileSync('/etc/letsencrypt/live/senso.senselive.io/privkey.pem', 'utf8'),
-//   cert: fs.readFileSync('/etc/letsencrypt/live/senso.senselive.io/fullchain.pem', 'utf8')
-// };
-
-// https.createServer(sslOptions, app).listen(PORT, HOST, () => {
-//   console.log(`✅ HTTPS Server running on https://${HOST}:${PORT}`);
+// app.listen(PORT, HOST, () => {
+//   console.log(`Server running on http://${HOST}:${PORT}`);
 // });
+require('dotenv').config();
+const fs = require('fs');
+const https = require('https');
+const app = require('./app');
+
+const PORT = process.env.PORT || 3300;
+const HOST = '0.0.0.0';
+
+const sslOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/senso.senselive.io/privkey.pem', 'utf8'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/senso.senselive.io/fullchain.pem', 'utf8')
+};
+
+https.createServer(sslOptions, app).listen(PORT, HOST, () => {
+  console.log(`✅ HTTPS Server running on https://${HOST}:${PORT}`);
+});
